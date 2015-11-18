750b54f639e1c663b641758e9acba1e8b1a7de20
MathieuVn <mathieu.vn@gmail.com>
2015-11-18 15:22:04 -0500

    Update Solution.py
    Add some comments

--- Algorithm/Largest-Rectangle-Aera/Solution.py
+++ Algorithm/Largest-Rectangle-Aera/Solution.py
@@ -13,28 +13,33 @@ class Solution(object):
         :type height: list[int]
         :rtype: int
         """
+        # Init max_area variable
         max_area = 0
 
+        # Case when height in empty
         if len(height) == 0:
             height = [0]
-        lenght = len(height)
-        half = max(int(round(lenght / 2, 0)), 1)
+        # Get the variable length and the integer half of length
+        length = len(height)
+        half = max(int(round(length / 2, 0)), 1)
 
+        # Flip the variable height to get the highest values near of the index 0 (left side)
         if sum(height[:half]) / half < sum(height[half:]) / half:
             height.reverse()
-        for i in range(lenght):
+        # For each columns (i.e. items of the list height)
+        for i in range(length):
             if i > 0 and height[i - 1] >= height[i]:
                 continue
             else:
                 mini_col = 0
-                for j in range(i, lenght):
+                for j in range(i, length):
                     if j == i:
                         mini_col = height[i]
                         max_area = max(max_area, mini_col)
                     elif height[j] < mini_col:
                         max_area = max(max_area, (j - i) * mini_col)
                         mini_col = height[j]
-                    elif j == lenght - 1:
+                    elif j == length - 1:
                         max_area = max(max_area, (j - i + 1) * mini_col)
                     else:
                         continue
